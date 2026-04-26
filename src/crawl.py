d8e2007e10c09237ded52b0d2d8e2b7b092843d8
nguyenhoangkhang-2010 <nguyenhoangkhang.241105@gmail.com>
2026-04-26 23:58:39 +0700

    update file crawl

--- src/crawl.py
+++ src/crawl.py
@@ -2,48 +2,42 @@ import cv2
 import os
 import glob
 
-def extract_frames_from_videos(video_folder, output_folder, total_images_needed=300):
-    video_paths = glob.glob(os.path.join(video_folder, "*.mp4"))
+def extract_optimal_frames(video_folder, output_folder, seconds_between_frames=2):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    video_dir = os.path.join(current_dir, "..", video_folder)
+    out_dir = os.path.join(current_dir, "..", output_folder)
+    
+    video_paths = glob.glob(os.path.join(video_dir, "*.mp4"))
     if not video_paths:
-        print("Không tìm thấy video nào!")
+        print(f"Không tìm thấy video .mp4 nào tại: {video_dir}")
         return
 
-    os.makedirs(output_folder, exist_ok=True)
-    images_per_video = total_images_needed // len(video_paths)
+    os.makedirs(out_dir, exist_ok=True)
     count = 0
 
-    print(f"🚀 Bắt đầu trích xuất {total_images_needed} ảnh từ {len(video_paths)} video...")
+    print(f"Bắt đầu vắt ảnh từ {len(video_paths)} video. Mỗi {seconds_between_frames} giây lấy 1 tấm!")
 
     for vid_path in video_paths:
         cap = cv2.VideoCapture(vid_path)
-        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        fps = round(cap.get(cv2.CAP_PROP_FPS)) 
         
-        step = max(1, total_frames // images_per_video)
+        step = fps * seconds_between_frames 
+        
         frame_id = 0
-        extracted_from_this_vid = 0
-
-        while cap.isOpened() and extracted_from_this_vid < images_per_video:
+        while cap.isOpened():
             ret, frame = cap.read()
             if not ret:
                 break
                 
             if frame_id % step == 0:
-                img_name = os.path.join(output_folder, f"traffic_frame_{count:04d}.jpg")
+                img_name = os.path.join(out_dir, f"huit_traffic_{count:04d}.jpg")
                 cv2.imwrite(img_name, frame)
                 count += 1
-                extracted_from_this_vid += 1
                 
             frame_id += 1
         cap.release()
 
-    print(f"✅ Hoàn tất! Đã lưu {count} ảnh vào {output_folder}")
+    print(f"Đã lưu {count} ảnh (chống trùng lặp 100%) vào {out_dir}")
 
 if __name__ == "__main__":
-    CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-    
-    VIDEO_DIR = os.path.join(CURRENT_DIR, "..", "data", "raw", "videos")
-    OUTPUT_DIR = os.path.join(CURRENT_DIR, "..", "data", "raw", "images")
-    
-    print(f"Đang tìm video tại: {VIDEO_DIR}")
-    
-    extract_frames_from_videos(VIDEO_DIR, OUTPUT_DIR)
+    extract_optimal_frames("data/raw/videos", "data/raw/images", seconds_between_frames=2)
