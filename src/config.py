734ff36a4c0afeec73fb51359bea0337ea0f260a
nguyenhoangkhang-2010 <nguyenhoangkhang.241105@gmail.com>
2026-05-20 15:51:27 +0700

    updated config,  utils and visualize_data

---
+++ src/config.py
@@ -0,0 +1,29 @@
+import os
+
+MODEL_PATH = "yolo26n.pt"
+VIDEO_INPUT = "data/raw/video-data-vehicles/"
+VIDEO_OUTPUT = "results/video/result_traffic.mp4"
+REPORT_OUTPUT = "results/traffic_frequency_report.csv"
+
+CONF_THRESHOLD = 0.45
+IMG_SIZE = 640
+
+MAX_AGE = 45
+N_INIT = 3
+MAX_COSINE_DIST = 0.2
+
+LINE_START = (100, 500)
+LINE_END = (1180, 500)
+
+TIME_WINDOW_SECONDS = 60
+
+CLASS_NAME = {
+    0: 'bike',
+    1: 'bus',
+    2: 'car',
+    3: 'motorbike',
+    4: 'plane',
+    5: 'ship',
+    6: 'train',
+    7: 'truck'
+}
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,13 @@
-import cv2
-import matplotlib.pyplot as plt
+import numpy as np
 
-def plot_boxes(img, boxes, labels):
-    for box, label in zip(boxes, labels):
-        x1, y1, x2, y2 = map(int, box)
-        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cv2.putText(img, label, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    return img
+def check_intersection(A, B, C, D):
+    def ccw(p1, p2, p3):
+        return (p3[1] - p1[1]) * (p2[0] - p1[0]) > (p2[1] - p1[1]) * (p2[0] - p1[0])
+    return ccw(A, C, D) != ccw(B, C, D) and ccw (A, B, C) != ccw(A, B, D)
+
+def get_direction(line_start, line_end, track_start, track_end):
+    v_line = np.array([line_end[0] - line_start[0], line_end[1] - line_start[1]])
+    v_move = np.array([track_end[0] - track_start[0], track_end[1] - track_start[1]])
+    
+    cross_product = np.cross(v_line, v_move)
+    return 1 if cross_product >= 0 else -1
